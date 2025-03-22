3b53252cd4213d5a7fc931feb07508479058418b
maymalinboim <may69761@gmail.com>
2025-03-22 18:38:05 +0200

    add https

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,22 @@
 import appPromise from "./app";
 import { Express } from "express";
+import http from "http"
+import https from "https"
+import fs from "fs"
 
 const PORT = process.env.PORT || 3000;
 
 appPromise.then((app: Express) => {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
+  if (process.env.NODE_ENV !== "production") {
+    console.log('development');
+    http.createServer(app).listen(PORT);
+  }
+  else {
+    console.log('production');
+    const options = {
+      key: fs.readFileSync("./client-key.pem"),
+      cert: fs.readFileSync("./client-cert.pem")
+    };
+    https.createServer(options, app).listen(PORT);
+  }
 });
